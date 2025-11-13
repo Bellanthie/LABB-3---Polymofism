7ab78d8d56230d34b6aa72bf2cb4257c8ee66128
Izabella <bellanthie@gmail.com>
2025-11-13 10:07:30 +0100

    cleaned up Geometry classs

--- LABB 3 - Polymofism/Geometry.cs
+++ LABB 3 - Polymofism/Geometry.cs
@@ -8,18 +8,10 @@ namespace LABB_3___Polymorfism
 {
     public class Geometry
     {
-        public string color { get; set; }
-        public bool isFilled { get; set; }
-
         // Virtual method to be overridden
         public virtual double Area()
         {
             return 0;
-        }
-
-        private static void Main(string[] args)
-        {
-
         }
     }
 }
